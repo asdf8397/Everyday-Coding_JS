d4ed86e8913449f9ca7d6912e9aacea239c9a168
asdf8397 <scv2314@naver.com>
2022-01-11 22:38:17 +0900

    2022.01.11

--- everycoding/배열/배열1/Demo.js
+++ everycoding/배열/배열1/Demo.js
@@ -1,3 +1,19 @@
 // 배열
 // 배열(array)연관된 데이터를 모아서 통으로 관리하기 위해서 사용하는 데이터타입
 
+// 배열의 생성
+// 간단한 데이터생성[워밍업]
+var team_member_name = "egoing";
+alert(team_member_name);
+console.log(team_member_name);
+
+/* 
+하나의 변수에 3개의 데이터를 담았다 각각의 데이터를 원소(Element)라고 부르고
+데이터를 뽑아오려면 어떻게 해야하는가
+*/
+var team_members = ["Baseball_team", "Soccer_team", "basketball"]
+/* 지정한 변수에 배열로 정리한 값들을 출력할땐 배열로 뽑아야한다 무조건 ()이 아닌
+[]로 뽑아야한다.*/
+console.log(team_members[0]);
+console.log(team_members[1]);
+console.log(team_members[2]);
