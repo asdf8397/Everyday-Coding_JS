86e6f843867d10b0d217a47d25d8ac77709d2147
asdf8397 <scv2314@naver.com>
2022-01-12 08:06:02 +0900

    2022.01.12

--- everycoding/배열/배열2/Demo.js
+++ everycoding/배열/배열2/Demo.js
@@ -1,15 +1,1 @@
-// 배열이 없다면
-function get_members() {
-    return "egoing"
-};
-document.write(get_members());
-
-function get_members2() {
-    return "k8805"
-};
-document.write(get_members2());
-
-function get_members3() {
-    return "algorithum"
-};
-document.write(get_members3());
+// 배열이 없다면...
